a3a206535d3e7cdb73804747cb6262b755c9314b
Favour Kehinde Madagu <115043659+Kndevaf@users.noreply.github.com>
2025-12-15 05:49:18 -0500

    Update mfa.py

--- auth/mfa.py
+++ auth/mfa.py
@@ -1,10 +1,8 @@
 import pyotp
 
-# Generate a TOTP secret for a user (do once)
 def generate_totp_secret():
     return pyotp.random_base32()
 
-# Verify a TOTP code
 def verify_totp(secret: str, token: str) -> bool:
     totp = pyotp.TOTP(secret)
     return totp.verify(token)
